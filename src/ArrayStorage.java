b79489e495e278d7530aa942b1adaab17c9626fa
Artem Zhuravlev <enigma.mrgreen@gmail.com>
2022-04-11 10:57:18 +0300

    Implementation of methods: save, get, delete, clear, getAll, size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        for (int to = size, i = size = 0; i < to; i++)
+            storage[i] = null;
     }
 
     void save(Resume r) {
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(storage[i].toString(), uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(storage[i].toString(), uuid)) {
+                for (int j = i; j < size; j++) {
+                    storage[j] = storage[j + 1];
+                    storage[j + 1] = null;
+                }
+                size -= 1;
+                break;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] temp = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            temp[i] = storage[i];
+        }
+        return temp;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
