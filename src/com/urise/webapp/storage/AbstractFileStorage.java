51fca47144499ca433bfa185d94b08cf722a5b3a
Artem Zhuravlev <enigma.mrgreen@gmail.com>
2022-05-24 12:53:22 +0300

    add AbstractFileStorage

---
+++ src/com/urise/webapp/storage/AbstractFileStorage.java
@@ -0,0 +1,101 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    public void clear() {
+        File[] list = directory.listFiles();
+        for (int i = 0; i < list.length; i++) {
+            list[i].delete();
+        }
+    }
+
+    @Override
+    public int size() {
+        File[] list = directory.listFiles();
+        return list.length;
+    }
+
+    @Override
+    protected Resume getResume(File file) {
+        try {
+            return doRead(file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected void setResume(File file, Resume r) {
+        try {
+            doWrite(r, file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    @Override
+    protected void deleteResume(File file) {
+        file.delete();
+    }
+
+    @Override
+    protected void saveResume(File file, Resume r) {
+        try {
+            file.createNewFile();
+            doWrite(r, file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    @Override
+    protected List<Resume> doGetAllSorted() {
+        File[] list = directory.listFiles();
+        List<Resume> resumeList = new ArrayList<>();
+        for (int i = 0; i < list.length; i++) {
+            try {
+                resumeList.add(doRead(list[i]));
+            } catch (IOException e) {
+                throw new StorageException("IO error", list[i].getName(), e);
+            }
+        }
+        return resumeList;
+    }
+
+    protected abstract void doWrite(Resume r, File file) throws IOException;
+
+    protected abstract Resume doRead(File file) throws IOException;
+
+}
